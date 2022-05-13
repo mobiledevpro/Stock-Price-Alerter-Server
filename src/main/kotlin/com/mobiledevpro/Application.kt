d5c10770af64d3a0539f79bd261fdf8b0d54f3f6
Dmitri Chernysh <dmitriy.chernysh@gmail.com>
2022-05-13 20:54:28 +0300

    Switch to HOCON configuration

--- src/main/kotlin/com/mobiledevpro/Application.kt
+++ src/main/kotlin/com/mobiledevpro/Application.kt
@@ -3,9 +3,20 @@ package com.mobiledevpro
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import com.mobiledevpro.plugins.*
+import io.ktor.server.application.*
 
+fun main(args: Array<String>): Unit = EngineMain.main(args)
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureRouting()
+}
+
+/*
 fun main() {
     embeddedServer(Netty, port = 8081, host = "0.0.0.0") {
         configureRouting()
     }.start(wait = true)
 }
+
+
+ */
