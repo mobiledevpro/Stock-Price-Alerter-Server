db08436f33aea8d408530b9151db4782b96e9e16
Dmitri Chernysh <dmitriy.chernysh@gmail.com>
2022-07-14 19:25:57 +0300

    Getting watchlist filled with price

--- src/main/kotlin/com/mobiledevpro/feature/crypto/userwatchlist/CryptoUserWatchlistMapper.kt
+++ src/main/kotlin/com/mobiledevpro/feature/crypto/userwatchlist/CryptoUserWatchlistMapper.kt
@@ -1,6 +1,7 @@
 package com.mobiledevpro.feature.crypto.userwatchlist
 
 import com.mobiledevpro.database.model.CryptoUserWatchlistTable
+import com.mobiledevpro.database.model.CryptoWatchlistTable
 import com.mobiledevpro.feature.crypto.userwatchlist.local.model.CryptoUserWatchlistTicker
 import com.mobiledevpro.feature.crypto.userwatchlist.remote.model.CryptoUserWatchlistRemote
 import org.jetbrains.exposed.sql.ResultRow
@@ -9,10 +10,13 @@ fun ResultRow.toCryptoUserWatchlistTicker(): CryptoUserWatchlistTicker =
     CryptoUserWatchlistTicker(
         userUid = this[CryptoUserWatchlistTable.userId],
         symbol = this[CryptoUserWatchlistTable.symbol],
+        lastPrice = this[CryptoWatchlistTable.lastPrice],
+        priceChange = this[CryptoWatchlistTable.priceChange],
+        priceChangePercent = this[CryptoWatchlistTable.priceChange],
+        updateTime = this[CryptoWatchlistTable.updateTime]
     )
-
 
 fun CryptoUserWatchlistTicker.toRemote(): CryptoUserWatchlistRemote =
     CryptoUserWatchlistRemote(
-        symbol
+        symbol, lastPrice, priceChange, priceChangePercent, updateTime
     )
